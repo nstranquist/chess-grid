ff5f53af9a35716119ccf03900b42009bcf86aaa
nico stran <nstranquist@gmail.com>
2019-04-18 13:35:27 -0500

    added script.js, made buttons clickable to toggle color change

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("toggle-bw").addEventListener("click", function() {
+    alert("black/white toggled");
+});
+document.getElementById("toggle-rb").addEventListener("click", function() {
+    alert("red/brown toggled");
+});
+document.getElementById("toggle-yg").addEventListener("click", function() {
+    alert("yellow/green toggled");
+});
